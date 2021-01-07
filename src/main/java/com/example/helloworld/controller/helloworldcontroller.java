3908bd1f5c2c9b537c4740027d91096f6ff784bc
chethanaRavichandran <67310472+chethanaRavichandran@users.noreply.github.com>
2021-01-07 18:51:01 +0530

    convention

--- src/main/java/com/example/helloworld/controller/helloworldcontroller.java
+++ src/main/java/com/example/helloworld/controller/helloworldcontroller.java
@@ -3,10 +3,10 @@ package com.example.helloworld.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class helloworldcontroller {
+@RestController // Annotation for creating Restful controllers
+public class HelloWorldController {
 
-    @GetMapping("/home")
+    @GetMapping("/") // Annotation maps HTTP GET requests onto specific handler methods
     public String hello() {
         return "hello world";
     }
