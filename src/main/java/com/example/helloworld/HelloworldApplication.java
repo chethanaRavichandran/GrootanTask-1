9fbfa26cb58f7aaa71165f8e1623ff7aad46fb14
chethanaRavichandran <67310472+chethanaRavichandran@users.noreply.github.com>
2021-01-07 14:47:34 +0530

    Add files via upload

---
+++ src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -0,0 +1,16 @@
+package com.example.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication //indicating the start of spring boot
+public class HelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldApplication.class, args); //initiating the application and server
+	}
+
+
+
+}
