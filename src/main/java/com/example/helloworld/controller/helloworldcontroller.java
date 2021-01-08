c9c119dc30ec5f8ed9dea521eafd397918c9236d
chethanaRavichandran <chethanaRavichandran@gmail.com>
2021-01-08 12:00:54 +0530

    task-2 completed

--- src/main/java/com/example/helloworld/controller/helloworldcontroller.java
+++ src/main/java/com/example/helloworld/controller/helloworldcontroller.java
@@ -1,13 +1,16 @@
 package com.example.helloworld.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class helloworldcontroller {
-
-    @GetMapping("/home")
+public class HelloWorldController {
+    @PostMapping("/")
     public String hello() {
-        return "hello world";
+        return "Hello world";
     }
+
+
 }
